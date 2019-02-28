bb48ad9a200eb8e36f5ecf3434de8dacd254b5cd
Brett Best <brettbest@live.com.au>
2019-03-01 06:19:08 +1100

    Added SwiftGTK

--- Package.swift
+++ Package.swift
@@ -5,11 +5,12 @@ import PackageDescription
 let package = Package(
   name: "Blinker",
   dependencies: [
-    .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.0.0")
+    .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.0.0"),
+    .package(url: "https://github.com/TomasLinhart/SwiftGtk", from: Version(0, 3, 1))
   ],
   targets: [
     .target(
       name: "Blinker",
-      dependencies: ["SwiftyGPIO"]),
+      dependencies: ["SwiftyGPIO", "SwiftGtk"]),
     ]
 )
