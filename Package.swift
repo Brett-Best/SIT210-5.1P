7b943a88e61839a6d5cd7456296cd819d23be3d4
Brett Best <brettbest@live.com.au>
2019-03-10 03:01:00 +1100

    Update GPIO handling, remove GTK.

--- Package.swift
+++ Package.swift
@@ -5,12 +5,11 @@ import PackageDescription
 let package = Package(
   name: "Blinker",
   dependencies: [
-    .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.0.0"),
-    .package(url: "https://github.com/TomasLinhart/SwiftGtk", from: Version(0, 3, 1))
+    .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.0.0")
   ],
   targets: [
     .target(
       name: "Blinker",
-      dependencies: ["SwiftyGPIO", "SwiftGtk"]),
+      dependencies: ["SwiftyGPIO"]),
     ]
 )
--- Sources/Blinker/main.swift
+++ Sources/Blinker/main.swift
@@ -4,6 +4,17 @@ import SwiftyGPIO
 let gp: GPIO = {
   let gpios = SwiftyGPIO.GPIOs(for: .RaspberryPi3)
   
+  gpios.forEach { e in
+    var value = e.value
+    
+    value.direction = .OUT
+    value.value = 1
+    
+    print(value.value)
+    
+    Thread.sleep(forTimeInterval: 0.5)
+  }
+  
   guard let gp = gpios[.P4] else {
     fatalError()
   }
@@ -18,29 +29,6 @@ signal(SIGINT) { _ in
 }
 
 gp.direction = .OUT
-
-
-import SwiftGtk
-
-let app = Application(applicationId: "com.example.application")
-app.run { window in
-  window.title = "Hello World"
-  window.defaultSize = Size(width: 400, height: 400)
-  window.resizable = true
-  
-  let button = Button(label: "Press Me")
-  button.clicked = { _ in
-    let newWindow = Window(windowType: .topLevel)
-    newWindow.title = "Just a window"
-    newWindow.defaultSize = Size(width: 200, height: 200)
-    let labelPressed = Label(text: "Oh, you pressed the button.")
-    newWindow.add(labelPressed)
-    
-    newWindow.showAll()
-  }
-  
-  window.add(button)
-}
 
 repeat {
   gp.value = (gp.value == 0) ? 1 : 0
